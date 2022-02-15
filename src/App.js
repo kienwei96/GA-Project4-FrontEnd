ce8a1413c720e6f34c8080d6d328a71f60050268
kienwei96 <kien_wei@hotmail.com>
2022-02-15 13:37:55 +0800

    include Authcontext and Fetchcontext to provide all the route

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useContext } from 'react';
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+  Switch,
+} from 'react-router-dom';
+import { Container } from '@material-ui/core';
+import { Button } from '@mui/material';
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from '@mui/material/styles';
+
+import Home from './components/home/Home';
+import NavBar from './components/layout/navbar/NavBar';
+import Footer from './components/layout/Footer';
+import Login from './components/login/Login';
+import { AuthProvider, AuthContext } from './context/AuthContext';
+import { FetchProvider } from './context/FetchContext';
+
+let theme = createTheme({
+  typography: {
+    fontFamily: 'Mulish, Arial',
+    body1: {
+      color: '#084c61',
+    },
+    button: {
+      textTransform: 'none',
+      fontSize: '15px',
+      padding: '5px',
+      marginLeft: '5px',
+      fontWeight: 600,
+    },
+  },
+  palette: {
+    primary: {
+      main: '#084C61',
+      light: '#a8acbd',
+      dark: '#a8acbd',
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <AuthProvider>
+          <FetchProvider>
+            <NavBar />
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/login' element={<Login />} />
+            </Routes>
+            <Footer />
+          </FetchProvider>
+        </AuthProvider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
