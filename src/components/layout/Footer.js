bb4adf512f925c24f2fda7db7d7683c239ba8b88
kienwei96 <kien_wei@hotmail.com>
2022-02-15 13:32:06 +0800

    create footer for the app

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { AppBar, Container, Grid, Link, Typography } from '@material-ui/core';
+
+export default () => {
+  return (
+    <footer>
+      <AppBar className='primary-color marginT-3 pad-2' position='static'>
+        <Grid component={Container} container>
+          <Grid item xs={12} sm={3}>
+            ConnectSport
+          </Grid>
+          <Grid item xs={12} sm={9} className='footer-links'>
+            <Link className='white-link' component={RouterLink} to='/'>
+              Home
+            </Link>
+            <span>/</span>
+            <Link className='white-link' component={RouterLink} to='/events'>
+              Events List
+            </Link>
+            <span>/</span>
+            <Link
+              className='white-link'
+              component={RouterLink}
+              to='/create-event'
+            >
+              Create Event
+            </Link>
+            <span>/</span>
+            <Link className='white-link' component={RouterLink} to='/register'>
+              Get Started
+            </Link>
+            <span>/</span>
+            <Link
+              className='white-link'
+              component={RouterLink}
+              to='/termsofservice'
+            >
+              Terms
+            </Link>
+            <span>/</span>
+            <Link
+              className='white-link'
+              component={RouterLink}
+              to='/privacypolicy'
+            >
+              Privacy
+            </Link>
+          </Grid>
+          <Grid item xs={12} sm={3}></Grid>
+          <Grid item xs={12} sm={9}>
+            <Typography className='marginL-d7'>
+              Copyright &copy;{new Date().getFullYear()} ConnectSport
+            </Typography>
+          </Grid>
+        </Grid>
+      </AppBar>
+    </footer>
+  );
+};
