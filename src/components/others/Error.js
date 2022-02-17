71d39de4401753d4c57b03b803eeef3664835904
kienwei96 <kien_wei@hotmail.com>
2022-02-17 18:26:59 +0800

    create loading or helper page

---
+++ src/components/others/Error.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <div>404. The page has an error.</div>;
+};
---
+++ src/components/others/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+export default () => {
+  return (
+    <CircularProgress
+      style={{ margin: 'auto', display: 'block' }}
+      size='10rem'
+    />
+  );
+};
