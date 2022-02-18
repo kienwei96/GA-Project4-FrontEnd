f275aad929e581472c6ac9f4f6b5136d6f335fe6
kienwei96 <kien_wei@hotmail.com>
2022-02-18 19:02:19 +0800

    refacotr profile code

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 import { Typography, Button } from '@material-ui/core';
 import { AuthContext } from '../../context/AuthContext';
 import { FetchContext } from '../../context/FetchContext';
+import { ProfileContext } from '../../context/ProfileContext';
+import ProfileContent from '../profileContent/profileContent';
 import Spinner from '../others/Spinner';
 import ErrorPage from '../others/Error';
-import ProfileContent from '../profileContent/profileContent';
 import styles from './profile.module.css';
 
 export default function Profile() {
   const authContext = useContext(AuthContext);
   const fetchContext = useContext(FetchContext);
+  const profileContext = useContext(ProfileContext);
   const [userProfile, setUserProfile] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
@@ -20,14 +22,16 @@ export default function Profile() {
       try {
         setLoading(true);
         const { data } = await fetchContext.authAxios.get(
-          `/profile/${authContext.authState.userInfo._id}`,
-          {}
+          `/profile/${authContext.authState.userInfo._id}`
         );
+        console.log('data', data);
         setUserProfile(data);
+        profileContext.setProfileState(data);
         setTimeout(() => {
           setLoading(false);
         }, 1500);
       } catch (error) {
+        setLoading(false);
         console.log(error);
         const { data } = error.response;
         setError(data.message);
@@ -37,10 +41,13 @@ export default function Profile() {
     getProfile();
   }, []);
 
-  let profilePage;
+  console.log('userProfile at profile.js', error);
+  console.log();
+
+  let profilePage = <ErrorPage />;
 
   try {
-    if (authContext.authState.userInfo.setProfile) {
+    if (error === 'Not found') {
       profilePage = (
         <div className={styles.profile}>
           <Typography variant='h5' component='p' gutterBottom>
@@ -62,23 +69,21 @@ export default function Profile() {
           </Link>
         </div>
       );
-    } else {
-      if (Object.keys(userProfile).length > 0) {
-        profilePage = <ProfileContent profileData={userProfile} />;
-      }
+    } else if (userProfile) {
+      profilePage = <ProfileContent profileData={userProfile} />;
     }
   } catch {
     profilePage = <ErrorPage />;
   }
-
-  console.log(userProfile);
   return (
     <>
       {loading ? (
         <Spinner />
       ) : (
-        <div className='minHeight'>
-          <h1 className='primary-textColor text-center'>User Profile</h1>
+        <div className={styles.content}>
+          <div className={styles.innerContent}>
+            <h1 className='primary-textColor text-center'>Profile</h1>
+          </div>
           {profilePage}
         </div>
       )}
