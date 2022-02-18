444313e8d53ab2d38e4a6af6fd4ff9c906048097
kienwei96 <kien_wei@hotmail.com>
2022-02-18 19:03:56 +0800

    update react route

--- src/App.js
+++ src/App.js
@@ -23,8 +23,14 @@ import Login from './components/login/Login';
 import Register from './components/register/Register';
 import Profile from './components/profile/profile';
 import CreateProfile from './components/createProfile/createProfile';
+import EditProfile from './components/editProfile/editProfile';
+import PrivacyPolicy from './components/others/PrivacyPolicy';
 import { AuthProvider, AuthContext } from './context/AuthContext';
 import { FetchProvider } from './context/FetchContext';
+import TermsofService from './components/others/TermsofService';
+import EventList from './components/findEvent/EventList';
+import CreateEvent from './components/createEvent/createEvent';
+import { ProfileProvider } from './context/ProfileContext';
 
 let theme = createTheme({
   typography: {
@@ -57,15 +63,34 @@ function App() {
       <Router>
         <AuthProvider>
           <FetchProvider>
-            <NavBar />
-            <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/login' element={<Login />} />
-              <Route exact path='/register' element={<Register />} />
-              <Route exact path='/profile' element={<Profile />} />
-              <Route exact path='/create-profile' element={<CreateProfile />} />
-            </Routes>
-            <Footer />
+            <ProfileProvider>
+              <NavBar />
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/login' element={<Login />} />
+                <Route exact path='/register' element={<Register />} />
+                <Route exact path='/profile' element={<Profile />} />
+                <Route exact path='/eventlist' element={<EventList />} />
+                <Route exact path='/createEvent' element={<CreateEvent />} />
+                <Route
+                  exact
+                  path='/create-profile'
+                  element={<CreateProfile />}
+                />
+                <Route exact path='/edit-profile' element={<EditProfile />} />
+                <Route
+                  exact
+                  path='/privacypolicy'
+                  element={<PrivacyPolicy />}
+                />
+                <Route
+                  exact
+                  path='/termsofservice'
+                  element={<TermsofService />}
+                />
+              </Routes>
+              <Footer />
+            </ProfileProvider>
           </FetchProvider>
         </AuthProvider>
       </Router>
