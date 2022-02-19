caf8b3f0557d1659897a9c4ce2a05a809a48beb8
kienwei96 <kien_wei@hotmail.com>
2022-02-19 17:15:29 +0800

    add responsive image

--- src/components/findEvent/EventsItem.js
+++ src/components/findEvent/EventsItem.js
@@ -21,10 +21,39 @@ import sportImage from '../../img/picture_2.jpg';
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import PersonAddDisabledIcon from '@mui/icons-material/PersonAddDisabled';
 import styles from './EventsItem.module.css';
+import Basketball from '../../img/basketball.jpg';
+import Baseball from '../../img/baseball.jpg';
+import Badminton from '../../img/badminton.jpg';
+import Football from '../../img/football.jpg';
+import PingPong from '../../img/pingpong.jpg';
+import Running from '../../img/running.jpg';
+import Soccer from '../../img/soccer.jpg';
+import Tennis from '../../img/tennis.jpg';
 
 export default function EventsItem(props) {
   const eventData = props.event;
   console.log('event item page props', eventData);
+
+  const photo = (sport) => {
+    switch (sport) {
+      case 'Basketball':
+        return Basketball;
+      case 'Baseball':
+        return Baseball;
+      case 'Badminton':
+        return Badminton;
+      case 'Football':
+        return Football;
+      case 'Ping Pong':
+        return PingPong;
+      case 'Running':
+        return Running;
+      case 'Soccer':
+        return Soccer;
+      case 'Tennis':
+        return Tennis;
+    }
+  };
   return (
     <>
       <div className={styles.outerCard}>
@@ -32,7 +61,7 @@ export default function EventsItem(props) {
           <Link to={`/event/${eventData._id}`}>
             <img
               style={{ width: '100%', height: '220px' }}
-              src={sportImage}
+              src={photo(eventData.sport)}
               alt='Sport'
             />
           </Link>
@@ -49,17 +78,13 @@ export default function EventsItem(props) {
             ) : (
               <Chip icon={<PersonAddAlt1Icon />} />
             )}
-
-            <Typography
-              className={styles.description}
-              variant='body2'
-              color='textSecondary'
-              component='p'
-            >
-              Invitation Slogan:
-              <br />
-              {eventData.description}
-            </Typography>
+            <div className={styles.description}>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                Invitation Slogan:
+                <br />
+                {eventData.description}
+              </Typography>
+            </div>
           </CardContent>
           <CardActions>
             <Button
