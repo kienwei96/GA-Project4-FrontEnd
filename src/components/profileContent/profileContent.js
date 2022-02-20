4cc199ea6611bd94a090ac2b0a60e70c2769139b
kienwei96 <kien_wei@hotmail.com>
2022-02-20 12:30:33 +0800

    update profile content page code to check user or other user

--- src/components/profileContent/profileContent.js
+++ src/components/profileContent/profileContent.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Grid, Card, CardContent, Chip } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Typography, Button } from '@material-ui/core';
 
 import Moment from 'react-moment';
@@ -10,26 +10,37 @@ import styles from './profile.module.css';
 export default function ProfileContent(props) {
   const authContext = useContext(AuthContext);
   const profileData = props.profileData.userProfile;
+  const profileCheck = props.profileCheck;
   const favoriteSport = profileData.favourite.map((sport, index) => (
     <Chip key={index} label={sport} />
   ));
   console.log('profile content props', props);
   console.log('profileData props', profileData);
 
+  const navigate = useNavigate();
+
+  const handleBackButton = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <div className={styles.content}>
-        <div className={styles.innerContent}>
-          <Link to='/edit-profile'>
-            <Button
-              className='primary-color marginB-2'
-              variant='contained'
-              to='/edit-profile'
-            >
-              Update Profile
-            </Button>
-          </Link>
-        </div>
+        {profileCheck ? (
+          <div className={styles.innerContent}>
+            <Link to='/edit-profile'>
+              <Button
+                className='primary-color marginB-2'
+                variant='contained'
+                to='/edit-profile'
+              >
+                Update Profile
+              </Button>
+            </Link>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
       <Grid className='marginB-2' container spacing={4}>
         <Grid item xs={12} sm={6}>
@@ -82,7 +93,18 @@ export default function ProfileContent(props) {
             </CardContent>
           </Card>
         </Grid>
+        <br />
       </Grid>
+      <div className={styles.backButton}>
+        <Button
+          className='primary-color white-link'
+          size='large'
+          variant='contained'
+          onClick={handleBackButton}
+        >
+          Back
+        </Button>
+      </div>
     </>
   );
 }
