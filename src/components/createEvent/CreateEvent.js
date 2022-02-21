368d1b55580088ef751c565d98ce7c227e16a563
kienwei96 <kien_wei@hotmail.com>
2022-02-21 13:20:49 +0800

    update create event page error

--- src/components/createEvent/CreateEvent.js
+++ src/components/createEvent/CreateEvent.js
@@ -46,11 +46,13 @@ export default function CreateEvent() {
           `/profile/${authContext.authState.userInfo._id}`,
           {}
         );
+        console.log('get profile success!');
         setUserProfile(data);
         setTimeout(() => {
           setLoading(false);
         }, 1500);
       } catch (error) {
+        console.log('get profile failed!');
         console.log(error);
         const { data } = error.response;
         setError(data.message);
@@ -59,6 +61,8 @@ export default function CreateEvent() {
 
     getProfile();
   }, []);
+
+  console.log('error is', error);
 
   const createEvent = async () => {
     try {
@@ -107,197 +111,194 @@ export default function CreateEvent() {
 
   let pageRedirect;
 
-  if (userProfile.setProfile) {
-    pageRedirect = <Navigate to='/profile' replace={true} />;
-  } else {
-    pageRedirect = (
-      <>
-        <Grid container justify='center' className='marginX-1'>
-          <Grid item xs={12} sm={8} md={6}>
-            <Card className='card'>
-              <CardContent>
-                <Typography
-                  variant='h3'
-                  component='h1'
-                  align='center'
-                  gutterBottom
-                >
-                  Host Your Event
-                </Typography>
-                <form className={styles.formContent}>
-                  <TextField
-                    margin='normal'
-                    label='Event Name *'
-                    placeholder=''
-                    name='eventname'
-                    type='name'
-                    value={eventName}
-                    onChange={(e) => {
-                      setEventName(e.target.value);
-                    }}
-                  />
-                  {response ? response.eventName : ''}
-                  <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                      <FormControl
-                        fullWidth={true}
-                        variant='standard'
-                        margin='normal'
+  pageRedirect = (
+    <>
+      <Grid container justify='center' className='marginX-1'>
+        <Grid item xs={12} sm={8} md={6}>
+          <Card className='card'>
+            <CardContent>
+              <Typography
+                variant='h3'
+                component='h1'
+                align='center'
+                gutterBottom
+              >
+                Host Your Event
+              </Typography>
+              <form className={styles.formContent}>
+                <TextField
+                  margin='normal'
+                  label='Event Name *'
+                  placeholder=''
+                  name='eventname'
+                  type='name'
+                  value={eventName}
+                  onChange={(e) => {
+                    setEventName(e.target.value);
+                  }}
+                />
+                {response ? response.eventName : ''}
+                <Grid container spacing={3}>
+                  <Grid item xs={6}>
+                    <FormControl
+                      fullWidth={true}
+                      variant='standard'
+                      margin='normal'
+                    >
+                      <InputLabel shrink={true} id='sportType'>
+                        Type of Sport *
+                      </InputLabel>
+                      <Select
+                        labelId='sportType'
+                        id='sportType'
+                        label='Type of Sport'
+                        value={sport}
+                        onChange={handleSportChange}
                       >
-                        <InputLabel shrink={true} id='sportType'>
-                          Type of Sport *
-                        </InputLabel>
-                        <Select
-                          labelId='sportType'
-                          id='sportType'
-                          label='Type of Sport'
-                          value={sport}
-                          onChange={handleSportChange}
-                        >
-                          <MenuItem value=''>
-                            <em>None</em>
-                          </MenuItem>
-                          <MenuItem value={'Badminton'}>Badminton</MenuItem>
-                          <MenuItem value={'Basketball'}>Basketball</MenuItem>
-                          <MenuItem value={'Baseball'}>Baseball</MenuItem>
-                          <MenuItem value={'Football'}>Football</MenuItem>
-                          <MenuItem value={'Futsal'}>Futsal</MenuItem>
-                          <MenuItem value={'Soccer'}>Soccer</MenuItem>
-                          <MenuItem value={'Ping Pong'}>Ping Pong</MenuItem>
-                          <MenuItem value={'Tennis'}>Tennis</MenuItem>
-                          <MenuItem value={'Running'}>Running</MenuItem>
-                        </Select>
-                        {response ? response.sport : ''}
-                      </FormControl>
-                    </Grid>
+                        <MenuItem value=''>
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={'Badminton'}>Badminton</MenuItem>
+                        <MenuItem value={'Basketball'}>Basketball</MenuItem>
+                        <MenuItem value={'Baseball'}>Baseball</MenuItem>
+                        <MenuItem value={'Football'}>Football</MenuItem>
+                        <MenuItem value={'Futsal'}>Futsal</MenuItem>
+                        <MenuItem value={'Soccer'}>Soccer</MenuItem>
+                        <MenuItem value={'Ping Pong'}>Ping Pong</MenuItem>
+                        <MenuItem value={'Tennis'}>Tennis</MenuItem>
+                        <MenuItem value={'Running'}>Running</MenuItem>
+                      </Select>
+                      {response ? response.sport : ''}
+                    </FormControl>
+                  </Grid>
 
-                    <Grid item xs={6}>
-                      <TextField
-                        fullWidth={true}
-                        margin='normal'
-                        label='Number of Player *'
-                        placeholder='2-20 Players'
-                        name='numberofplayer'
-                        type='number'
-                        onChange={(e) => {
-                          setPlayer(e.target.value);
-                        }}
-                      />
-                      {response ? response.player : ''}
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                      <FormControl
-                        fullWidth={true}
-                        variant='outlined'
-                        margin='normal'
-                      >
-                        <InputLabel
-                          shrink={true}
-                          id='demo-simple-select-filled-label'
-                        >
-                          COVID-19 Vaccination Required *
-                        </InputLabel>
-                        <Select
-                          label='Vaccination Required *'
-                          name='vaccination'
-                          type='name'
-                          value={vaccination}
-                          onChange={handleVaccinationChange}
-                        >
-                          <MenuItem value={'Yes'}>Yes</MenuItem>
-                          <MenuItem value={'No'}>No</MenuItem>
-                        </Select>
-                        {response ? response.vaccination : ''}
-                      </FormControl>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <FormControl
-                        fullWidth={true}
-                        variant='outlined'
-                        margin='normal'
-                      >
-                        <InputLabel
-                          shrink={true}
-                          id='demo-simple-select-filled-label'
-                        >
-                          Game Level *
-                        </InputLabel>
-                        <Select
-                          label='Game Level *'
-                          name='level'
-                          type='name'
-                          value={level}
-                          onChange={handleLevelChange}
-                        >
-                          <MenuItem value={'Easy'}>Easy</MenuItem>
-                          <MenuItem value={'Medium'}>Medium</MenuItem>
-                          <MenuItem value={'Hard'}>Hard</MenuItem>
-                        </Select>
-                        {response ? response.level : ''}
-                      </FormControl>
-                    </Grid>
-                  </Grid>
-                  <TextField
-                    label='Location'
-                    placeholder='Details about this event'
-                    name='location'
-                    type='name'
-                    value={location}
-                    onChange={(e) => {
-                      setLocation(e.target.value);
-                    }}
-                  />
-                  {response ? response.location : ''}
-                  <FormControl margin='normal'>
+                  <Grid item xs={6}>
                     <TextField
-                      type='date'
-                      name='start'
-                      label='Start Date'
-                      value={startDate}
+                      fullWidth={true}
+                      margin='normal'
+                      label='Number of Player *'
+                      placeholder='2-20 Players'
+                      name='numberofplayer'
+                      type='number'
                       onChange={(e) => {
-                        setStartDate(e.target.value);
-                      }}
-                      variant='outlined'
-                      InputLabelProps={{
-                        shrink: true,
+                        setPlayer(e.target.value);
                       }}
                     />
-                    {response ? response.startDate : ''}
-                  </FormControl>
+                    {response ? response.player : ''}
+                  </Grid>
+                </Grid>
+                <Grid container spacing={3}>
+                  <Grid item xs={6}>
+                    <FormControl
+                      fullWidth={true}
+                      variant='outlined'
+                      margin='normal'
+                    >
+                      <InputLabel
+                        shrink={true}
+                        id='demo-simple-select-filled-label'
+                      >
+                        COVID-19 Vaccination Required *
+                      </InputLabel>
+                      <Select
+                        label='Vaccination Required *'
+                        name='vaccination'
+                        type='name'
+                        value={vaccination}
+                        onChange={handleVaccinationChange}
+                      >
+                        <MenuItem value={'Yes'}>Yes</MenuItem>
+                        <MenuItem value={'No'}>No</MenuItem>
+                      </Select>
+                      {response ? response.vaccination : ''}
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <FormControl
+                      fullWidth={true}
+                      variant='outlined'
+                      margin='normal'
+                    >
+                      <InputLabel
+                        shrink={true}
+                        id='demo-simple-select-filled-label'
+                      >
+                        Game Level *
+                      </InputLabel>
+                      <Select
+                        label='Game Level *'
+                        name='level'
+                        type='name'
+                        value={level}
+                        onChange={handleLevelChange}
+                      >
+                        <MenuItem value={'Easy'}>Easy</MenuItem>
+                        <MenuItem value={'Medium'}>Medium</MenuItem>
+                        <MenuItem value={'Hard'}>Hard</MenuItem>
+                      </Select>
+                      {response ? response.level : ''}
+                    </FormControl>
+                  </Grid>
+                </Grid>
+                <TextField
+                  label='Location'
+                  placeholder='Details about this event'
+                  name='location'
+                  type='name'
+                  value={location}
+                  onChange={(e) => {
+                    setLocation(e.target.value);
+                  }}
+                />
+                {response ? response.location : ''}
+                <FormControl margin='normal'>
                   <TextField
-                    label='Description'
-                    placeholder='Details about this event'
-                    name='description'
-                    type='name'
-                    value={description}
+                    type='date'
+                    name='start'
+                    label='Start Date'
+                    value={startDate}
                     onChange={(e) => {
-                      setDescription(e.target.value);
+                      setStartDate(e.target.value);
                     }}
-                    multiline
-                    rows={5}
+                    variant='outlined'
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
                   />
-                  <Button
-                    className='primary-color marginB-2'
-                    type='submit'
-                    variant='contained'
-                    fullWidth
-                    onClick={handleSubmit}
-                  >
-                    Submit
-                  </Button>
-                </form>
-              </CardContent>
-            </Card>
-          </Grid>
+                  {response ? response.startDate : ''}
+                </FormControl>
+                <TextField
+                  label='Description'
+                  placeholder='Details about this event'
+                  name='description'
+                  type='name'
+                  value={description}
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
+                  multiline
+                  rows={5}
+                />
+                <Button
+                  className='primary-color marginB-2'
+                  type='submit'
+                  variant='contained'
+                  fullWidth
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
         </Grid>
-      </>
-    );
-  }
+      </Grid>
+    </>
+  );
 
   return (
     <>
+      {error.length > 1 && <Navigate to='/profile' replace={true} />}
       {redirectPage && <Navigate to='/eventmain' replace={true} />}
       {loading ? <Spinner /> : pageRedirect}
     </>
